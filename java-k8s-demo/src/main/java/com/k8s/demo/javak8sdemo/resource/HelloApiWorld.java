085786c4f66a8a0a544415d5515cdec2102f8eaf
bokjo <bstojcevski89@gmail.com>
2018-11-19 19:33:52 +0100

    Added whitelisting CORS for localhost testing pupouses for the Java Spring Boot stuff

--- java-k8s-demo/src/main/java/com/k8s/demo/javak8sdemo/resource/HelloApiWorld.java
+++ java-k8s-demo/src/main/java/com/k8s/demo/javak8sdemo/resource/HelloApiWorld.java
@@ -3,6 +3,7 @@ package com.k8s.demo.javak8sdemo.resource;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +18,7 @@ public class HelloApiWorld {
     String hostname;
     HostInfo greeting;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/k8s")
     @ResponseBody
     public HostInfo hello () {
