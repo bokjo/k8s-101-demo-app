5fb31870e32bb4858b655d6839e9bb441c83646b
Bojanche Stojchevski <Bojanche.Stojchevski@endava.com>
2018-11-20 17:10:52 +0100

    Added version in the NodeJS api response and a few headers for the cross origin

--- nodejs-k8s-demo/src/server.js
+++ nodejs-k8s-demo/src/server.js
@@ -4,6 +4,7 @@ const dns = require("dns");
 
 const port = 3000;
 const app = express();
+const version = "v1";
 
 var ip;
 
@@ -11,11 +12,22 @@ const lookup = dns.lookup(os.hostname(), function(err, address, family) {
     ip = address;
 });
 
+app.use(function(req, res, next) {
+    res.header("Content-Type", "application/json");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    res.header("Access-Control-Max-Age", "3600");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/v1/api/nodejs/k8s", function(req, res) {     
     console.log("IP OUT: " + ip);
     res.json({
+        "version": version,
         "ip": ip, 
-        "host": os.hostname()
+        "hostname": os.hostname()
     })
 })
 
